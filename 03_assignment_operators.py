3b8fb8d1d7d1608a0b3db1bfde631e14efb3c1d4
Jed Peterson <jed.peterson@exceleng.net>
2023-06-08 18:33:04 -0500

    add operator demos

---
+++ 03_assignment_operators.py
@@ -0,0 +1,27 @@
+# demonstrates assignment operators
+
+a = 5
+
+# addition
+a += 1 
+print(a) # 6
+
+# subtraction
+a -= 1
+print(a) # 5
+
+# multiplication
+a *= 3
+print(a) # 15
+
+# division
+a /= 5
+print(a) # 3
+
+# exponential
+a **= 3
+print(a) # 9
+
+# modulus
+a %= 2
+print(a) # 1
---
+++ 04_math_operators.py
@@ -0,0 +1,23 @@
+# The following demos basic math operators.
+
+a = 2
+b = 3
+
+# addition
+print(a + b)
+
+# subtraction
+print(a - b)
+
+# divison
+print(a / b)
+# NOTE: result is automatically converted to float type
+
+# multiplication
+print(a * b)
+
+# exponential
+print(a ** b)
+
+# modulus
+print(a % b)
